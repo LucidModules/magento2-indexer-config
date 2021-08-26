7724a300b427e42fec6331e651c917f8551a8ffa
Maciej Chadala <maciejcha@gmail.com>
2021-08-26 19:11:59 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Module LucidModules_IndexerConfig
+This module optimizes Magento 2 indexer batch size. Use it as a template for your e-commerce and experiment with batch size to match your needs.
---
+++ registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Copyright © Lucid Modules. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'LucidModules_IndexerConfig',
+    __DIR__
+);
